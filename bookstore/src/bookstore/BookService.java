cb8951a22e35e6dca2bd032d6ef245d5dab5f476
MorickClive <MorickClive@Gmail.com>
2017-08-31 10:24:17 +0100

    pushing Json Read/Write (limited)

--- bookstore/src/bookstore/BookService.java
+++ bookstore/src/bookstore/BookService.java
@@ -1,12 +1,20 @@
 package bookstore;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+
+import com.fasterxml.jackson.core.JsonEncoding;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 
 public class BookService {
 
 	private static int totalBooks = 0;
 	
-	HashMap<Integer,Book> bookList;
+	private HashMap<Integer,Book> bookList;
 	
 	public BookService(){
 		bookList = new HashMap<>();
@@ -46,6 +54,77 @@ public class BookService {
 				 System.out.println("\t- ID: " + x + "\t" + bookList.get(x).printMe());
 			}
 		}
+		else{	System.out.println("No books to list.");		}
 		
 	}
+
+	public Book getBook(int ID){return bookList.get(ID);}
+	
+	public void extractToJSON(String filelocation){
+		try {
+			JsonFactory factory = new JsonFactory();
+			JsonGenerator jGenerator = factory.createGenerator(new File(
+					filelocation),
+					JsonEncoding.UTF8);
+			Book bkHandle = null;
+			
+			for(int x = 1; x <= bookList.size(); x++){
+				// active book
+				bkHandle = bookList.get(x);
+				
+				// Start writing object
+				jGenerator.writeStartObject(); // {	
+				
+				jGenerator.writeStringField("Title", bkHandle.getTitle()); // "name" : "mkyong"
+				jGenerator.writeStringField("Author", bkHandle.getAuthor()); // "name" : "mkyong"
+				jGenerator.writeNumberField("Cost", bkHandle.getCost()); // "age" : 29
+				
+				jGenerator.writeEndObject(); // }
+			}
+
+			jGenerator.close();
+		} catch (IOException e) {
+			System.out.println("WARNING: Failed to write to Json!!!");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}	
+
+	public void readTargJSON(String filelocation, int totalentries){
+
+			try {
+				JsonFactory factory = new JsonFactory();
+				JsonParser jParser = factory.createJsonParser(new File(filelocation));	
+				
+				// need to adjust this to read until end of file.
+				for(int x = 0; x < totalentries; x++){
+					while (jParser.nextToken() != JsonToken.END_OBJECT) {
+						String fieldname = jParser.getCurrentName();
+						if ("Title".equals(fieldname)) {
+						  jParser.nextToken();
+						  System.out.println(jParser.getText()); // display mkyong
+	
+						}
+						if ("Author".equals(fieldname)) {
+							  jParser.nextToken();
+							  System.out.println(jParser.getText()); // display mkyong
+	
+							}
+						if ("Cost".equals(fieldname)) {
+							  jParser.nextToken();
+							  System.out.println(jParser.getFloatValue()); // display mkyong
+	
+							}
+					}
+				}
+				jParser.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("WARNING: FAILED TO READ JSON!!!");
+			}
+	}
+
+	
 }
--- bookstore/src/bookstore/Main.java
+++ bookstore/src/bookstore/Main.java
@@ -10,8 +10,17 @@ public class Main {
 		
 		library.addBook(new Book("Rats", "author", 2.5f));
 		library.addBook(new Book("Adventures of Riverside", "T. R. Rogers", 7.5f));	
+		library.addBook(new Book("", "", 0f));
+		
+		Book bk = library.getBook(3);
+		
+		bk.setTitle("Galaxy away");
+		bk.setAuthor("Jeremy Maxwell");
+		bk.setCost(12.5f);
 		
 		library.listBooks();
+		library.extractToJSON("C:\\JSONWRITE\\book.json");
+		library.readTargJSON("C:\\JSONWRITE\\book.json", 3);
 		
 		
 		library.removeBook(0);
